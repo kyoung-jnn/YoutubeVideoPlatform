36a6c6df9695d7acb6a7b4faaed25217c798b267
Rohkyoungjin <rudwls468@gmail.com>
2020-12-26 23:48:53 +0900

    Router 추가작업

--- src/components/SubVideo.js
+++ src/components/SubVideo.js
@@ -4,17 +4,21 @@ import YouTube from "react-youtube";
 
 const SubVideo = (props) => {
   const subVideos = props.subVideos;
+  const setMainVideo = props.setMainVideo;
 
   const renderVideoList = subVideos.map((video) => {
     return (
       <Grid>
-        <Paper style={{ cursor: "pointer", marginBottom:'20px'}}>
+        <Paper style={{ cursor: "pointer" }}>
           <img
             style={{ width: "100%" }}
             alt="thumbnail"
             src={video.snippet.thumbnails.medium.url}
+            onMouseDown={() => setMainVideo(video)}
           />
-          <Typography variant="subtitle">{video.snippet.title}</Typography>
+        </Paper>
+        <Paper style={{ padding: "10px", marginBottom: "20px" }}>
+          <Typography variant="h5" style={{fontSize:'6px'}}>{video.snippet.title}</Typography>
         </Paper>
       </Grid>
     );
--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { makeStyles, Grid } from "@material-ui/core";
-import { Route, Link } from "react-router-dom";
 import axios from "axios";
 import SearchBar from "./components/SearchBar.js";
 import MainVideo from "./components/MainVideo.js";
@@ -13,6 +12,7 @@ const useStyles = makeStyles((theme) => ({
 function App() {
   const [videos, setVideos] = useState([]);
   const [currentVideo, setCurrentVideo] = useState(null);
+
   const handleSubmit = async (searchKeyword) => {
     axios
       .create({
@@ -32,6 +32,23 @@ function App() {
         setVideos(list);
         setCurrentVideo(list[0]);
       });
+
+    console.log(currentVideo);
+
+    axios
+      .create({
+        baseURL: "https://www.googleapis.com/youtube/v3/",
+      })
+      .get("videos", {
+        params: {
+          part: "snippet,contentDetails,recordingDetails",
+          key: "AIzaSyA_6zSz_6DWbstFSBOs_4gncohnIsdAoXk", // api 키
+          id: currentVideo.id.videoId,
+        },
+      })
+      .then((results) => {
+        console.log(results);
+      });
   };
 
   return (
@@ -40,6 +57,15 @@ function App() {
         <Grid container spacing={3} style={{ justifyContent: "center" }}>
           <Grid item xs={12}>
             <SearchBar onSubmit={handleSubmit}></SearchBar>
+          </Grid>
+          <Grid item xs={6}>
+            <MainVideo mainVideo={currentVideo}></MainVideo>
+          </Grid>
+          <Grid item xs={3}>
+            <SubVideo
+              subVideos={videos}
+              setMainVideo={setCurrentVideo}
+            ></SubVideo>
           </Grid>
         </Grid>
       </Grid>
