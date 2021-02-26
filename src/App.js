171b546c53705d8df8d1b086bc76234558b78651
Rohkyoungjin <rudwls468@gmail.com>
2021-02-26 18:24:27 +0900

    feat: 썸네일 불러오기 제작중

--- src/App.js
+++ src/App.js
@@ -3,53 +3,64 @@ import { Grid } from "@material-ui/core";
 import { Route, Switch, useHistory } from "react-router-dom";
 import axios from "axios";
 import SearchBar from "./components/SearchBar.js";
-import VideoDetail from "./components/VideoDetail.js";
+import VideoDetail from "./components/VideoDetail_component/VideoDetail.js";
 import VideoMenu from "./components/VideoMenu.js";
 
 function App() {
-  const [videos, setVideos] = useState([]); // 비디오들 정보 전부 저장
+  const [menuState, setMenuState] = useState({ videos: [], channels: [] }); // 비디오들 정보 전부 저장
   const [currentVideo, setCurrentVideo] = useState(null); // id만 저장
+  var channelList = [];
 
   const history = useHistory();
 
   // componentDidMount
   // VideoMenu에 인기 동영상 불러오기
   useEffect(() => {
-    console.log("videos", videos);
-    console.log("currentVideo", currentVideo);
+    console.log("비디오 탐색");
+    console.log(menuState);
     axios
       .create({
         baseURL: "https://www.googleapis.com/youtube/v3/",
       })
       .get("/videos", {
         params: {
-          part: "snippet,statistics,recordingDetails",
+          part: "snippet,statistics,recordingDetails,contentDetails",
           chart: "mostPopular",
           maxResults: 12, // 가져올 동영상 개수
           regionCode: "kr",
-          key: "AIzaSyAWgs3aZE3PyX2p0tL776GoBgMt3XNx71M", // api 키
+          key: process.env.REACT_APP_YOUTUBE_API_KEY, // api 키
         },
       })
       .then((results) => {
         const list = results.data.items;
+        setMenuState({ videos: list, ...menuState });
+      })
+      .then(() => {
+        console.log("채널 탐색");
+        console.log(menuState);
 
-        setVideos(list);
+        menuState.videos.map((video) => {
+          let channelId = video.snippet.channelId;
+          axios
+            .create({
+              baseURL: "https://www.googleapis.com/youtube/v3/",
+            })
+            .get("/channels", {
+              params: {
+                part: "snippet",
+                id: { channelId },
+                key: "AIzaSyAWgs3aZE3PyX2p0tL776GoBgMt3XNx71M", // api 키
+              },
+            })
+            .then((results) => {
+              channelList.push(results.data);
+              if (channelList.length == 12) {
+                console.log("채널 저장", channelList);
+                setMenuState({ channels: channelList, ...menuState });
+              }
+            });
+        });
       });
-
-    // axios
-    //   .create({
-    //     baseURL: "https://www.googleapis.com/youtube/v3/",
-    //   })
-    //   .get("/channels", {
-    //     params: {
-    //       part: "snippet.thumbnails",
-    //       id: "UCZf4ZESHAIuRtZ-eoMSL97A",
-    //       key: "AIzaSyAWgs3aZE3PyX2p0tL776GoBgMt3XNx71M", // api 키
-    //     },
-    //   })
-    //   .then((results) => {
-    //     console.log(results)
-    //   });
   }, []);
 
   const handleGoHome = () => {
@@ -67,14 +78,14 @@ function App() {
         params: {
           part: "snippet",
           maxResults: 12, // 가져올 동영상 개수
-          key: "AIzaSyA_6zSz_6DWbstFSBOs_4gncohnIsdAoXk", // api 키
+          key: process.env.REACT_APP_YOUTUBE_API_KEY, // api 키
           q: searchKeyword,
         },
       })
       .then((results) => {
         const list = results.data.items;
 
-        setVideos(list);
+        setMenuState(list);
       });
   };
 
@@ -89,23 +100,23 @@ function App() {
       <Grid item xs={12}>
         <SearchBar onGoHome={handleGoHome} onSubmit={handleSubmit}></SearchBar>
       </Grid>
-      <Grid container xs={12} spacing={0} justify="space-around">
+      <Grid container justify="space-around">
         <Switch>
           <Route
             exact
             path="/"
-            render={(props) => (
-              <VideoMenu
-                videos={videos}
-                onClickVideo={handleClick}
-                {...props}
-              ></VideoMenu>
-            )}
+            // render={(props) => (
+            //   <VideoMenu
+            //     videos={videos}
+            //     onClickVideo={handleClick}
+            //     {...props}
+            //   ></VideoMenu>
+            // )}
           ></Route>
-          <Route
+          {/* <Route
             path="/videoDetail"
             render={(props) => <VideoDetail {...props}></VideoDetail>}
-          ></Route>
+          ></Route> */}
         </Switch>
       </Grid>
     </Grid>
--- src/components/VideoMenu.js
+++ src/components/VideoMenu.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
-import { Paper, Typography, Grid, makeStyles } from "@material-ui/core";
+import { Paper, Typography, Grid, makeStyles, Avatar } from "@material-ui/core";
 
 const Video = (props) => {
-  const videos = props.videos;
-  console.log("videoMenu", videos);
+  const videos = props.videos.videos;
   const onClickVideo = props.onClickVideo;
+  console.log(videos)
 
   const classes = useStyles();
 
   const renderVideoMenu = videos.map((video) => {
     return (
-      <Grid item xs={3}>
+      <Grid item md={3} sm={4} xs={6}>
         <div className={classes.video}>
           <img
             style={{ position: "relative", width: "100%", cursor: "pointer" }}
@@ -24,19 +24,36 @@ const Video = (props) => {
               }
             }}
           />
-          <Typography style={{ textAlign: "center" }} variant="body1">
-            {video.snippet.title}
-          </Typography>
-          <Typography
-            style={{
-              marginTop: "10px",
-              textAlign: "right",
-              fontWeight: "bold",
-            }}
-            variant="subtitle2"
-          >
-            {video.snippet.channelTitle}
-          </Typography>
+          <Grid container xs className={classes.videoText}>
+            <Grid item xs={2}>
+              <Avatar></Avatar>
+            </Grid>
+            <Grid item xs>
+              <Grid>
+                <Typography className={classes.videoTitleText}>
+                  {video.snippet.title}
+                </Typography>
+              </Grid>
+              <Grid style={{ marginTop: 10 }}>
+                <Typography
+                  style={{
+                    textAlign: "left",
+                  }}
+                  variant="subtitle2"
+                >
+                  {video.snippet.channelTitle}
+                </Typography>
+                <Typography
+                  style={{
+                    textAlign: "left",
+                  }}
+                  variant="subtitle2"
+                >
+                  조회수 {video.statistics.viewCount}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
         </div>
       </Grid>
     );
@@ -47,8 +64,24 @@ const Video = (props) => {
 
 const useStyles = makeStyles((theme) => ({
   video: {
-    padding: theme.spacing(2),
+    marginTop: theme.spacing(3),
+    padding: theme.spacing(1),
   },
+  videoTitleText: {
+    display: "-webkit-flex",
+    WebkitLineClamp: 2,
+    WebkitBoxOrient: "vertical",
+
+    fontSize: 14,
+    height: "3em",
+    lineHeight: 1.5,
+    textAlign: "left",
+    fontWeight: "bold",
+
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+  },
+  videoText: {},
 }));
 
 export default Video;
